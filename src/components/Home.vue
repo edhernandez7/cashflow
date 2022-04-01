5de110433ef1f72a2653b145116327402da47912
Danilo Hernandez <dhernandez0032@gmail.com>
2022-04-01 05:14:33 -0600

    header y contenido

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,20 @@
 <template>
-  <div>Hola mundo</div>
+  <Layout>
+    <template #header>
+      <Header></Header>
+    </template>
+  </Layout>
 </template>
 
 <script>
+import Layout from '@/components/Layout.vue';
+import Header from '@/components/Header.vue';
+
 export default {
   name: "HomeApp",
+  components: {
+    Layout,
+    Header,
+  },
 };
 </script>
